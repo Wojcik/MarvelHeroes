1057b5e3bcf8ddb948b8929f7d7e9e67cfeade0a
Anton Kovalev <kovalev.main@gmail.com>
2014-10-31 12:45:27 +0200

    TEST

--- Marvel/Constants.m
+++ Marvel/Constants.m
@@ -2,6 +2,7 @@
 // Created by Anton Kovalev on 5/21/14.
 // Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
 //
+//
 
 NSString * const kBaseURL = @"http://gateway.marvel.com/v1/public/";
 NSString * const kKeyForPublicAPIKey = @"apikey";
